6cff1ef282cef6222e6466b87ca3ea9c6da2129d
Lazaro <otienolazaro111@gmail.com>
2026-06-09 10:31:22 +0300

    Adding another method and renaming variable

--- checkSquare.js
+++ checkSquare.js
@@ -1,8 +1,15 @@
 function isSquare(n) {
     if(n<0) return false;
-    let integ = Math.sqrt(n);
-    return console.log(Number.isInteger(integ));
+    let squareRoot = Math.sqrt(n);
+    return console.log(Number.isInteger(squareRoot));
 
 }
 
 isSquare(110);
+
+function isSquare(n) {
+    return console.log(Math.sqrt(n) % 1 === 0);
+}
+
+isSquare(110);
+
