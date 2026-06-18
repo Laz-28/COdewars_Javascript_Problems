3b90b98202a903f7a26749df77a41a92bead18ae
Lazaro <otienolazaro111@gmail.com>
2026-06-18 07:32:54 +0300

    Add a shorter alternative using indexOf

--- findNeele.js
+++ findNeele.js
@@ -1,4 +1,4 @@
-function findNeedle(haystack) {
+/*function findNeedle(haystack) {
  
   for(let i = 1; i <= haystack.length; i++){
     if(haystack[i] === "needle"){
@@ -6,5 +6,10 @@ function findNeedle(haystack) {
       return "found the needle at position " + i;
     }
   }
+}*/
+
+function findNeedle(haystack){
+    return `found the needle at position ${haystack.indexOf("needle") + 1}`
 }
 
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]))
