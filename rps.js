1562a767a7968aa3fbb8cf2dd8953409085962fd
Lazaro <otienolazaro111@gmail.com>
2026-06-23 09:48:22 +0300

    Rock paper scissor game

---
+++ rps.js
@@ -0,0 +1,15 @@
+const rps = (p1, p2) => {
+    p1 = p1.toLowerCase();
+    p2 = p2.toLowerCase();
+  if(p1 === "scissors" && p2 ==="paper" || p2 === "scissors" && p1 ==="rock" || p1 === "paper" && p2==="rock"){
+    return "Player 1 won!";
+}else if(p1 === "scissors" && p2 ==="rock" || p1 === "paper" && p2 ==="scissors" || p2 === "paper" && p1 === "rock" ){
+    return "Player 2 won!";
+}else if(p1 === "paper" && p2 === "paper" || p1 === "rock" && p2 === "rock" || p1 ==="scissors" && p2 === "scissors"){
+    return "Draw!";
+}else{
+  return "Invalid Inputs!";
+}
+};
+
+console.log(rps("Rock","scissors"));
